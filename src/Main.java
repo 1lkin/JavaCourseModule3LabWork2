ef6a9d6b04266370dc723dfe6a2f314969f3aa9d
Ilkin <ilkinhasanov673@gmail.com>
2021-04-07 20:51:40 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Ilkin Hasanov
+ * <p>
+ * Copyright (c) HASANOV.
+ */
+
+/**
+ * IKIN HASANOV's response to Homework [x], Module [x].
+ *
+ *   Task:
+ *
+ *
+ * @version 1.10
+ * @Since [DD] [MMM] [YYYY]
+ * @class
+ * @author Ilkin Hasanov
+ */
+public class Main {
+}
